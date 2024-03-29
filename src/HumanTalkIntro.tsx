ca2e31582dda5a859a786863fd8a27a2dd3d8c38
Alexis Lozano <alexis.pascal.lozano@gmail.com>
2024-03-29 22:44:34 +0100

    chore: remove Sequence & AbsoluteFill from HumanTalkIntro

--- src/HumanTalkIntro.tsx
+++ src/HumanTalkIntro.tsx
@@ -1,4 +1,3 @@
-import { AbsoluteFill, Sequence } from "remotion";
 import { Background, Logo, Title, Speakers, EventInfo } from "./components";
 import { Talk } from "./talk";
 
@@ -6,47 +5,10 @@ export const HumanTalkIntro = (talk: Talk) => {
   return (
     <>
       <Background />
-
-      <Sequence>
-        <AbsoluteFill style={{
-          alignItems: "center",
-          top: "8%",
-        }}>
-          <Logo />
-        </AbsoluteFill>
-      </Sequence>
-
-      <Sequence from={15}>
-        <AbsoluteFill style={{
-          alignItems: "center",
-          top: "30%",
-        }}>
-          <div style={{
-            width: "80%"
-          }}>
-            <Title text={talk.title}/>
-          </div>
-        </AbsoluteFill>
-      </Sequence>
-
-      <Sequence from={30}>
-        <AbsoluteFill style={{
-          alignItems: "center",
-          top: "55%",
-
-        }}>
-          <Speakers speakers={talk.speakers} />
-        </AbsoluteFill>
-      </Sequence>
-
-      <Sequence from={38}>
-        <AbsoluteFill style={{
-          alignItems: "center",
-          top: "90%"
-        }}>
-          <EventInfo {...talk.info} />
-        </AbsoluteFill>
-      </Sequence>
+      <Logo />
+      <Title text={talk.title}/>
+      <Speakers speakers={talk.speakers} />
+      <EventInfo {...talk.info} />
     </>
   );
 };
